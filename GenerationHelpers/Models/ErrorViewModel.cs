d3b5855e43996c6770f8ddc16ecbda0d9514a984
Johnatan Lima <joh23nathan@gmail.com>
2018-10-05 22:07:50 -0300

    Trabalhando com alguns tipos de Helpers

---
+++ GenerationHelpers/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GenerationHelpers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
