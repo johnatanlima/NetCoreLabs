4f8392bfb93256b9048552a56b802244e58a98aa
userDexter Fedora29 <joh23nathan@gmail.com>
2019-07-26 04:12:39 -0300

    Mudanças em alguns labs e adicao de PROJETO CONCLUSAO

--- Tutorial_Lab_5/Start/MyWebApp/Controllers/HomeController.cs
+++ Tutorial_Lab_5/Start/MyWebApp/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-using System;
-using System.IO;
-using System.Linq;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyWebApp.Controllers
@@ -17,14 +13,14 @@ namespace MyWebApp.Controllers
             _environment = env;
         }
         
-        /*
         public IActionResult Index() {
  
-            return View(DataSource.Countries);
+            return View();
         }
 
+        /*
         //ADD
-
+        
         [HttpGet]
         public IActionResult UpdateNationalFlag(string code) {
         var country = DataSource.Countries.SingleOrDefault(c => c.Code.Equals
